ff745355485b3ee831e7ef86cd588e6592fdbdfe
Lenovo <Lenovo@DESKTOP-1DD019O>
2023-02-08 15:59:09 +0530

    Add project files.

---
+++ Interface/ICustomer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    internal interface ICustomer
+    {
+        public string GetCustomerName()
+        {
+            return "Geetha";
+        }
+        void GetCustomerInfo();
+    }
+    class Customer : ICustomer
+    {
+        public int GetCustomerId() 
+        {
+            return 103;
+
+        }
+    }
+    public void GetCustomerInfo()
+    {
+        Console.WriteLine("get customer info from interface implemented in customer class");
+    }
+
+
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer=new Customer();
+            customer.GetCustomerInfo();
+            Console.WriteLine(customer.GetCustomerId());
+            Console.WriteLine(((ICustomer)customer).GetCustomerName());
+
+        }
+    }
+}
